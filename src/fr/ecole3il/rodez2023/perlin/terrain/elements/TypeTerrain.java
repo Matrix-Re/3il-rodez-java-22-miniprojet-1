cc901b3b9cd061ce63a4f955f5a0efe40b56d0f3
Anas AMIRI <amiria@3il.fr>
2024-01-09 22:49:50 +0100

    Rédaction de la méthode getImage pour quelle retourne l'image associer à sont type

--- src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
@@ -1,11 +1,46 @@
 package fr.ecole3il.rodez2023.perlin.terrain.elements;
 
+import fr.ecole3il.rodez2023.perlin.Utils;
+
 import java.awt.image.BufferedImage;
 
 public enum TypeTerrain {
     PLAINE, FORET_FEUILLUS, TOUNDRA, FORET_CONIFÈRES, MONTAGNE, DESERT, COLLINES, MARAIS, OCEAN;
 
     public BufferedImage getImage() {
-        return null;
+
+        BufferedImage image = null;
+
+        switch (this.name()){
+            case "PLAINE":
+                image = Utils.chargerTuile("plain.png");
+                break;
+            case "FORET_FEUILLUS":
+                image = Utils.chargerTuile("deciduous_forest.png");
+                break;
+            case "TOUNDRA":
+                image = Utils.chargerTuile("tundra.png");
+                break;
+            case "FORET_CONIFÈRES":
+                image = Utils.chargerTuile("coniferous_forest.png");
+                break;
+            case "MONTAGNE":
+                image = Utils.chargerTuile("mountain.png");
+                break;
+            case "DESERT":
+                image = Utils.chargerTuile("desert.png");
+                break;
+            case "COLLINES":
+                image = Utils.chargerTuile("hills.png");
+                break;
+            case "MARAIS":
+                image = Utils.chargerTuile("marsh.png");
+                break;
+            case "OCEAN":
+                image = Utils.chargerTuile("ocean.png");
+                break;
+        }
+
+        return image;
     }
 }
